d0802ffda6e7251bfe1775760e12cbfa4ce91021
cbb246 <825048@qq.com>
2022-10-15 16:13:13 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oUHz16OkU1IULUdwgXtgfzaVzNoE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '小陈老公在提醒您~~',
+      useTemplateId: '9xp8dcpNw32DOHS9EwgjIw1kXgf8OdI39kOsYwub9eA	',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-11',
       festivals: [
